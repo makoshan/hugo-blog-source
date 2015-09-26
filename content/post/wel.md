479e866f5b101781b189dc4743bf127b3376f501
ghui <hi@ghui.me>
2015-09-26 19:12:30 +0800

    init

---
+++ content/post/wel.md
@@ -0,0 +1,10 @@
++++
+Categories = ["Development", "GoLang"]
+Description = ""
+Tags = ["Development", "golang"]
+date = "2015-09-26T19:01:21+08:00"
+menu = "main"
+title = "wel"
+
++++
+
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t slim# if using a theme, replace by `hugo -t <yourtheme>`
+# if using a theme, replace by `hugo -t <yourtheme>`
+hugo
 
 # Go To Public folder
 cd /Users/ghui/.hugo_blog
